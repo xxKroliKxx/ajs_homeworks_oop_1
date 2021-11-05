8e4165580afd20939f4a22e09f2d0291c2c64303
rka <russkikh_k@ekb.sima-land.ru>
2021-11-05 17:38:32 +0500

    +

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,62 +1,66 @@
-import {Bowman, Swordsman, Magician, Daemon, Undead, Zombie} from '../js/app';
+import {
+  Bowman, Swordsman, Magician, Daemon, Undead, Zombie,
+} from '../js/app';
 import Character from '../js/character';
 
-
 test('character name length test', () => {
-    expect(() => {
-        new Character("1", 'Magician')
-    }).toThrow(new Error('incorrect name length'));
+  expect(() => {
+    /* eslint-disable no-new */
+    new Character('1', 'Magician');
+  }).toThrow(new Error('incorrect name length'));
 });
 
 test('character type test', () => {
-    expect(() => {
-        new Character("test1", "incorrectType")
-    }).toThrow(new Error('type not found'));
+  expect(() => {
+    /* eslint-disable no-new */
+    new Character('test1', 'incorrectType');
+  }).toThrow(new Error('type not found'));
 });
 
 test('character get name', () => {
-    let name = 'test1'
-    expect(new Character(name, 'Magician').name).toBe(name);
+  const name = 'test1';
+  expect(new Character(name, 'Magician').name).toBe(name);
 });
 
 test('character get type', () => {
-    let type = 'Magician'
-    expect(new Character('name', type).type).toBe(type);
+  const type = 'Magician';
+  expect(new Character('name', type).type).toBe(type);
 });
 
-
 test.each([
-    {name: 'Bowman', type: 'Bowman'},
-    {name: 'Swordsman', type: 'Swordsman'},
-    {name: 'Magician', type: 'Magician'},
-    {name: 'Daemon', type: 'Daemon'},
-    {name: 'Undead', type: 'Undead'},
-    {name: 'Zombie', type: 'Zombie'}
+  { name: 'Bowman', type: 'Bowman' },
+  { name: 'Swordsman', type: 'Swordsman' },
+  { name: 'Magician', type: 'Magician' },
+  { name: 'Daemon', type: 'Daemon' },
+  { name: 'Undead', type: 'Undead' },
+  { name: 'Zombie', type: 'Zombie' },
 ])(('characters test'),
-    (object) => {
-        let f = () => {
-            let element
-            switch (object.name) {
-                case 'Bowman':
-                    element = new Bowman(object.name)
-                    break
-                case 'Swordsman':
-                    element = new Swordsman(object.name)
-                    break
-                case 'Magician':
-                    element = new Magician(object.name)
-                    break
-                case 'Daemon':
-                    element = new Daemon(object.name)
-                    break
-                case 'Undead':
-                    element = new Undead(object.name)
-                    break
-                case 'Zombie':
-                    element = new Zombie(object.name)
-                    break
-            }
-        }
-        expect(f).not.toThrow();
-    }
-)
+  (object) => {
+    /* eslint-disable no-unused-vars */
+    const f = () => {
+      let element;
+      switch (object.name) {
+        case 'Bowman':
+          element = new Bowman(object.name);
+          break;
+        case 'Swordsman':
+          element = new Swordsman(object.name);
+          break;
+        case 'Magician':
+          element = new Magician(object.name);
+          break;
+        case 'Daemon':
+          element = new Daemon(object.name);
+          break;
+        case 'Undead':
+          element = new Undead(object.name);
+          break;
+        case 'Zombie':
+          element = new Zombie(object.name);
+          break;
+        default:
+          break;
+      }
+    };
+    expect(f).not.toThrow();
+  });
--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,37 @@
 import character from './character';
 
 export class Bowman extends character {
-    constructor(name) {
-        super(name, 'Bowman');
-    }
+  constructor(name) {
+    super(name, 'Bowman');
+  }
 }
 
 export class Swordsman extends character {
-    constructor(name) {
-        super(name, 'Swordsman');
-    }
+  constructor(name) {
+    super(name, 'Swordsman');
+  }
 }
 
 export class Magician extends character {
-    constructor(name) {
-        super(name, 'Magician');
-    }
+  constructor(name) {
+    super(name, 'Magician');
+  }
 }
 
 export class Daemon extends character {
-    constructor(name) {
-        super(name, 'Daemon');
-    }
+  constructor(name) {
+    super(name, 'Daemon');
+  }
 }
 
 export class Undead extends character {
-    constructor(name) {
-        super(name, 'Undead');
-    }
+  constructor(name) {
+    super(name, 'Undead');
+  }
 }
 
 export class Zombie extends character {
-    constructor(name) {
-        super(name, 'Zombie');
-    }
+  constructor(name) {
+    super(name, 'Zombie');
+  }
 }
